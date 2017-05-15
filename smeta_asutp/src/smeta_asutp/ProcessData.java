d8b6161fa44f123dc6030bc90d4a9949fb1a09bb
vodnovol <vodnovol@C031.spets-stroy.com>
2017-05-15 16:57:12 +0300

    First commit

---
+++ smeta_asutp/src/smeta_asutp/ProcessData.java
@@ -0,0 +1,15 @@
+package smeta_asutp;
+
+public class ProcessData {
+
+	public static void main(String[] args) {
+		// 1 создать инстанс ProcessData
+		// 2 собрать входные данные, с диска, бд, и через экранную форму
+		
+
+	}
+	public void createInputForm(){
+		// создать JForm с layout'ом.
+		
+	}
+}
